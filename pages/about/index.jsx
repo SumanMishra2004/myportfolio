f10842cfbd1082e6f2db887a4e413a2a2ee90c41
sumanMishra <suman132j@gmail.com>
2024-05-06 21:09:50 +0530

    about page index change final

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -18,64 +18,7 @@ import {
   SiCanva
 } from "react-icons/si";
 
-const aboutData = [
-  {
-    title: "skills",
-    info: [
-      {
-        title: "Web Development",
-        icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-         <SiMongodb/>,
-         
-        ],
-      },
-      {
-        title: "UI/UX Design",
-        icons: [<FaFigma />,<SiCanva/>],
-      },
-    ],
-  },
-  {
-    title: "Topic",
-    info: [
-      {
-        title: "UX/UI Designer - XYZ Company",
-        stage: "2012 - 2023",
-      },
-      {
-        title: "Web Developer - ABC Agency",
-        stage: "2010 - 2012",
-      },
-      {
-        title: "Intern - DEF Corporation",
-        stage: "2008 - 2010",
-      },
-    ],
-  },
-  {
-    title: "credentials",
-    info: [
-      {
-        title: "Web Development - ABC University, LA, CA",
-        stage: "2011",
-      },
-      {
-        title: "Computer Science Diploma - AV Technical Institute",
-        stage: "2009",
-      },
-      {
-        title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
-        stage: "2006",
-      },
-    ],
-  },
-];
+
 const index = () => {
   return (
     <div>index</div>
