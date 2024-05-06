f88137bdfc2edb04829fde3f1bd0b6c245bc28fe
sumanMishra <suman132j@gmail.com>
2024-05-06 21:06:01 +0530

    about page index

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const index = () => {
+// icons
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaFigma,
+} from "react-icons/fa";
+
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+  SiMongodb,
+  SiCanva
+} from "react-icons/si";
+import Avatar from "../../components/Avatar";
+import Circles from "../../components/Circles";
+import CountUp from "react-countup";
+//  data
+const aboutData = [
+  {
+    title: "skills",
+    info: [
+      {
+        title: "Web Development",
+        icons: [
+          <FaHtml5 />,
+          <FaCss3 />,
+          <FaJs />,
+          <FaReact />,
+          <SiNextdotjs />,
+          <SiFramer />,
+         <SiMongodb/>,
+         
+        ],
+      },
+      {
+        title: "UI/UX Design",
+        icons: [<FaFigma />,<SiCanva/>],
+      },
+    ],
+  },
+  {
+    title: "Topic",
+    info: [
+      {
+        title: "UX/UI Designer - XYZ Company",
+        stage: "2012 - 2023",
+      },
+      {
+        title: "Web Developer - ABC Agency",
+        stage: "2010 - 2012",
+      },
+      {
+        title: "Intern - DEF Corporation",
+        stage: "2008 - 2010",
+      },
+    ],
+  },
+  {
+    title: "credentials",
+    info: [
+      {
+        title: "Web Development - ABC University, LA, CA",
+        stage: "2011",
+      },
+      {
+        title: "Computer Science Diploma - AV Technical Institute",
+        stage: "2009",
+      },
+      {
+        title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
+        stage: "2006",
+      },
+    ],
+  },
+];
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+const About = () => {
+ 
   return (
-    <div>index</div>
-  )
-}
+   <>
+   index
+   </>
+  );
+};
 
-export default index
+export default About;
